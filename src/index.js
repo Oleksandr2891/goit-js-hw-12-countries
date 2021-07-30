295d1e33825c1cfec87686508c4d27ab0fb628d8
Oleksandr2891 <allavrenko@gmail.com>
2021-07-30 20:36:34 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,52 +1,83 @@
 import './sass/main.scss';
+import _ from "lodash";
+import cardCountry from './tmp/card-country.hbs';
+import listCountries from './tmp/list-countries.hbs';
+import { alert, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import { defaults } from '@pnotify/core';
+
+ 
+defaults.addModalClass = 'pnotify-window';
+defaults.icon = null;
+defaults.maxTextHeight = null;
+defaults.shadow = true;
+defaults.closer = true;
 
 
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-    '#FFCC33',
-    '#FF0000',
-    '#CC33CC',
-    '#0000CC',
-    '#33FFFF',
-    '#00FF66',
-    '#336666',
-    '#330066',
-    '#CC0033',
-    '#FFFF00',
-    '#FF00CC',
-    '#999900',
-];
+const searchWrapper = document.querySelector('.search-wrapper');
+const input = document.querySelector('input');
+const buttonSearch = document.querySelector('.button-search');
 
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+const renderPage = function (html) {
+    searchWrapper.innerHTML = html;
 };
 
-const buttonStart = document.querySelector('button[data-action="start"]');
-const buttonStop = document.querySelector('button[data-action="stop"]');
-const backgroundForColors = document.querySelector('body');
-const intervalChange = 1000;
-let pressButton = true;
+const initEvent = function () {
+    document.querySelector('#product-catalog').addEventListener('click', (e) => {
+        if (e.target.tagName !== "P") return false
+        onFetch(e.target.textContent);
+    });
+};
 
-let intervalId;
+input.addEventListener('input', _.debounce(onInputSearch, 700));
 
-
-
-buttonStart.addEventListener('click', () => {
-    if (!pressButton) return;
-    pressButton = false;
-    intervalId = setInterval(() => {
-        backgroundForColors.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
-        console.log("работает");
-    }, intervalChange);
+buttonSearch.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'BUTTON') return false;
+    console.log(event);
+    event.preventDefault();
+    searchWrapper.innerHTML = "";
+    input.value = "";
 });
 
-buttonStop.addEventListener('click', () => {
-    pressButton = true;
-    clearInterval(intervalId);
-    console.log("интервал очищено");
-});
+
+
+function onInputSearch(event) {
+    let nameSearch = "";
+    if (event.target.value !== "") {
+        nameSearch = event.target.value;
+        onFetch(nameSearch);
+    } else {
+        onFetch();
+    }
+    return;
+};
+
+
+function onFetch(name) {
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then(response =>  response.ok ? response.json() : Promise.reject(response))
+        .then(country => {
+            if (country.length === 1) renderPage(cardCountry(country[0]));
+            else if (country.length > 1 && country.length < 11) {
+                renderPage(listCountries(country));
+                initEvent();
+            }
+            else if (country.length > 10) {
+                alert({
+                    text: "Вам необходимо уточнить запрос",
+                    type: 'info'
+                  })
+            }
+         })
+        .catch(() => {
+            alert({
+                    text: "По вашему запросу ничего не найдено!",
+                    type: 'info'
+                  })
+        });
+}
+
+
+
